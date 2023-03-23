27e596d1e69c57fe3dc07c029bb5cfeb5de1ce95
giorgia-dit <giorgia.ditano@gmail.com>
2023-03-23 12:37:26 +0100

    first commit

---
+++ R/read-data.R
@@ -0,0 +1,14 @@
+# LOAD PACKAGES
+
+library(tidyverse)
+
+# LOAD DATA
+
+mlw_path <- here::here('data/mlw-data.csv')
+mlw_ds <- read_csv(mlw_path)
+
+mlw_meta_path <- here::here('data/mlw-meta.csv')
+mlw_meta <- read_csv(mlw_meta_path)
+
+olm_path <- here::here('data/olm-data.csv')
+olm_ds <- read_csv(mlw_path)
